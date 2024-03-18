112d75d6e5bb6b51ad2b18719d5657656376beb2
Ilyasov Ainur <ainurka55555@mail.ru>
2024-03-18 18:36:40 +0300

    fix: remove example 0

--- lib/examples/example_0.dart
+++ lib/examples/example_0.dart
@@ -10,37 +10,6 @@ class Example0 extends StatelessWidget {
           title: const Text('AppBar'),
           backgroundColor: Colors.blue,
         ),
-        body: ColoredBox(
-          color: Colors.yellowAccent,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(
-                height: 50,
-                width: 50,
-                child: ColoredBox(
-                  color: Colors.black54,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                height: 100,
-                width: 100,
-                color: Colors.black54,
-                child: const ColoredBox(
-                  color: Colors.blue,
-                ),
-              ),
-              const Text(
-                'Hello World!',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 30,
-                ),
-              ),
-            ],
-          ),
-        ));
+        body: Container());
   }
 }
