bab8e318cfa78e7796d1aaf9b1ae9045b7b0abd9
Ilyasov Ainur <ainurka55555@mail.ru>
2024-03-27 21:19:49 +0300

    feat: add expample 0

--- lib/examples/example_0.dart
+++ lib/examples/example_0.dart
@@ -7,9 +7,47 @@ class Example0 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('AppBar'),
-          backgroundColor: Colors.blue,
+          title: const Text('Text App Bar'),
+          backgroundColor: Colors.yellow,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add_alert),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.add_a_photo),
+              onPressed: () {},
+            ),
+          ],
         ),
-        body: Container());
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+              child: const Text(
+                'Hello',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.green,
+            ),
+          ],
+        ));
   }
 }
